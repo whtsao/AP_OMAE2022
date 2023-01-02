25dacd5695362a5fe1a359130c3dd72f5d96490a
Wen-Huai Tsao <87154971+whtsao@users.noreply.github.com>
2023-01-02 13:37:36 -0600

    Add files via upload

---
+++ free_vib/proteus_wall/floating_body.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+#SBATCH -N 2
+#SBATCH -n 48
+#SBATCH -c 1 # specify 6 threads per process
+#SBATCH -t 72:00:00
+#SBATCH -p workq
+#SBATCH -A loni_proteus01r
+#SBATCH -o o.out # optional, name of the stdout, using the job number (%j) and the first node (%N)
+#SBATCH -e e.err
+# below are job commands
+date
+
+export WORKDIR=/work/$USER
+
+module purge
+module load proteus/1.7.5
+
+mkdir -p $WORKDIR/Pro_freevib_wall.$SLURM_JOBID
+cd $WORKDIR/Pro_freevib_wall.$SLURM_JOBID 
+cp $SLURM_SUBMIT_DIR/floating_body.py .
+cp $SLURM_SUBMIT_DIR/petsc.options.superlu_dist .
+cp $SLURM_SUBMIT_DIR/floating_body.sh .
+
+srun -N2 -n48 -c1 parun --TwoPhaseFlow floating_body.py -l 5 -C "he=0.01" -O petsc.options.superlu_dist
+
+date
+
+exit 0
+
